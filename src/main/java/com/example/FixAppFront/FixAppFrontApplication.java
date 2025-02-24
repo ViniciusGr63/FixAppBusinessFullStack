6ff69c4e5529af4dd28aa6a4fb6b9ff6e5c934df
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2025-02-24 17:04:17 +0000

    unzip

---
+++ src/main/java/com/example/FixAppFront/FixAppFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.FixAppFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixAppFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixAppFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FixAppFront/FixAppFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FixAppFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixAppFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
